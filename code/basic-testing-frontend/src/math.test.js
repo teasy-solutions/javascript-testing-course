487ec272a3a78e93f62a22ed56238da6d0ffcba3
Yury Alencar <yury.alencar@condoconta.com.br>
2022-09-27 17:22:12 -0300

    new tests

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -1,42 +1,41 @@
 import { it, expect } from 'vitest'
 import { add } from './math'
 
-it('teste de soma usando apenas números ex 1', () => {
-  expect(4).toBe(add([2,2]))
-})
-
-it('teste de soma usando apenas números ex 2', () => {
+it('test soma numeros do metodo add', () => {
+  // Triple AAA
   // Arrange
-  const numbers = [2,2]
+  const numbers = [10 ,2, 2, 3]
 
   // Act
   const result = add(numbers)
 
   // Assert
-  expect(result).toBe(4)
-})
-
-it('teste de soma usando apenas números ex 3', () => {
-  // Arrange
-  const numbers = [2,2]
-
-  // Act
-  const result = add(numbers)
-
-  // Assert
-  const expectedResult = numbers.reduce((prev, curr) => prev + curr , 0)
+  const expectedResult = numbers.reduce((prev, curr) => prev + curr, 0)
   expect(result).toBe(expectedResult)
 })
 
-it('Retornar NaN caso tenha algum número inválido', () => {
-  const numbers = ['invalid', 1]
+it('Retornar NaN quando um número for inválido', () => {
+  const numbers = [2, 'invalid']
   const result = add(numbers)
   expect(result).toBeNaN()
 })
 
-it('Somar corretamente caso tenha String com número', () => {
-  const numbers = ['1', '2']
+it('Somar corretamente, quando o número for string', () => {
+  const numbers = [2, '2', '4', '20']
   const result = add(numbers)
-  const expectedResult = numbers.reduce((prev, curr) => +prev + +curr , 0)
-  expect(result).toBe(expectedResult) 
+  const expectedResult = numbers.reduce((prev, curr) => +prev + +curr, 0)
+  expect(result).toBe(expectedResult)
+})
+
+it('Se eu não passar nada precisa dar uma exceção', () => {
+  const resultFunction = () => add()
+  expect(resultFunction).toThrow(/is not iterable/)
+})
+
+it('Múltiplos parâmetros', () => {
+  const number1 = 2
+  const number2 = 3
+
+  const resultFunction = () => add(number1, number2)
+  expect(resultFunction).toThrow(/is not iterable/)
 })
---
+++ code/basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,3 @@
+import { it, expect } from 'vitest'
+import { transformToNumber } from './numbers'
+
