c9abd77ee27a6923116f3eb112e5f4995d073cd2
Yury Alencar <yury.alencar@condoconta.com.br>
2022-10-04 17:22:16 -0300

    integration tests

--- code/basic-testing-frontend/src/util/numbers.test.js
+++ code/basic-testing-frontend/src/util/numbers.test.js
@@ -1,20 +1,36 @@
-import { it, expect } from 'vitest'
-import { transformToNumber } from './numbers'
+import { describe, it, expect } from 'vitest'
+import { cleanNumbers, transformToNumber } from './numbers'
 
-it('Número inválido', () => {
-  const number = 'invalid'
-  const result = transformToNumber(number)
-  expect(result).toBeNaN()
+describe('transformToNumber()', () => {
+  it('Número inválido', () => {
+    const number = 'invalid'
+    const result = transformToNumber(number)
+    expect(result).toBeNaN()
+  })
+  
+  it('Conversão de um número string em número', () => {
+    const number = '2'
+    const result = transformToNumber(number)
+    expect(result).toBeTypeOf('number')
+  })
+  
+  it('Conversão de um número já numérico', () => {
+    const number = 2
+    const result = transformToNumber(number)
+    expect(result).toBeTypeOf('number')
+  })
 })
 
-it('Conversão de um número string em número', () => {
-  const number = '2'
-  const result = transformToNumber(number)
-  expect(result).toBeTypeOf('number')
-})
+describe('cleanNumbers()', () => {
+  it('Sempre é retornado um array de numeros se informado um array de string com numeros', () => {
+    const numberValues = ['1', '2']
+    const result = cleanNumbers(numberValues)
+    expect(result[0]).toBeTypeOf('number')
+  })
 
-it('Conversão de um número já numérico', () => {
-  const number = 2
-  const result = transformToNumber(number)
-  expect(result).toBeTypeOf('number')
+  it('Erro se informar número inválido', () => {
+    const numberValues = ['', 2]
+    const resultFunction = () => cleanNumbers(numberValues)
+    expect(resultFunction).toThrow()
+  })
 })
