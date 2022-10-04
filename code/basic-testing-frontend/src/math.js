47d2d85f67dee4585c9e5526841febd05ad68547
Yury Alencar <yury.alencar@condoconta.com.br>
2022-10-03 22:10:53 -0300

    add good tests module

--- code/basic-testing-frontend/src/math.js
+++ code/basic-testing-frontend/src/math.js
@@ -1,3 +1,5 @@
+import { cleanNumbers } from "./util/numbers.js";
+
 export function add(numbers) {
   let sum = 0;
 
@@ -6,4 +8,17 @@ export function add(numbers) {
   }
 
   return sum;
+}
+
+export function calculateResult (numberValues) {
+  let result = ''
+
+  try {
+    const numbers = cleanNumbers(numberValues);
+    result = add(numbers).toString();
+  } catch (error) {
+    result = error.message;
+  }
+
+  return result
 }
